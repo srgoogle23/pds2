00c04ba994164af3623387df5e48465cc5adafaa
wsl_srgoogle23 <leonardoliveira@ufmg.br>
2022-04-07 09:33:46 -0300

    feat: adicionado comentários explicativos 2

--- vector/banco/Banco.cpp
+++ vector/banco/Banco.cpp
@@ -4,27 +4,56 @@
 
 using namespace std;
 
-// cria uma classe de agencia que se relaciona com a classe banco
 class Agencia {
 	private:
 		string numero;
 		string nome;
 		string endereco;
 	public:
+		/**
+		 * @brief Construtor padrão do objeto Agencia
+		 * 
+		 * @param numero 
+		 * @param nome 
+		 * @param endereco 
+		 */
 		Agencia(string numero, string nome, string endereco) {
 			this->numero = numero;
 			this->nome = nome;
 			this->endereco = endereco;
 		}
+
+		/**
+		 * @brief Get the Numero object
+		 * 
+		 * @return string 
+		 */
 		string getNumero() {
 			return numero;
 		}
+
+		/**
+		 * @brief Get the Nome object	
+		 * 
+		 * @return string 
+		 */
 		string getNome() {
 			return nome;
 		}
+
+		/**
+		 * @brief Get the Endereco object
+		 * 
+		 * @return string 
+		 */
 		string getEndereco() {
 			return endereco;
 		}
+
+		/**
+		 * @brief Método que imprime todas as informações da agência
+		 * 
+		 */
 		void print() {
 			cout << "Numero: " << numero << endl;
 			cout << "Nome: " << nome << endl;
@@ -37,12 +66,28 @@ class Banco {
 		string nome;
 		vector<Agencia> agencias;
 	public:
+		/**
+		 * @brief Construct a new Banco object
+		 * 
+		 * @param nome 
+		 */
 		Banco(string nome) {
 			this->nome = nome;
 		}
+
+		/**
+		 * @brief Adiciona uma nova agência ao banco
+		 * 
+		 * @param agencia 
+		 */
 		void addAgencia(Agencia agencia) {
 			agencias.push_back(agencia);
 		}
+
+		/**
+		 * @brief Imprime todas as agências do banco
+		 * 
+		 */
 		void print() {
 			cout << "Banco: " << nome << endl << endl;
 			for (int i = 0; i < agencias.size(); i++) {
