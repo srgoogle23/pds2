8b2a25f2f598bd3055fe3540354d02de7d780132
wsl_srgoogle23 <leonardoliveira@ufmg.br>
2022-04-07 10:18:21 -0300

    docs: adicionado documentação do tad fracao

--- tipos abstratos de dados/exercicios/tads/TadFracao/fracao.cpp
+++ tipos abstratos de dados/exercicios/tads/TadFracao/fracao.cpp
@@ -6,6 +6,11 @@ using namespace std;
 /**
  * cria uma classe de manipulação de frações,
  * onde executará operações de adição, subtração, multiplicação e divisão.
+ * 
+ * obs.: criei 3 construtores:
+ * 1 - sem parâmetros (inicializa com 0/1)
+ * 2 - com um parâmetro (inicializa com o valor passado, um inteiro/1)
+ * 3 - com dois parâmetros (inicializa com o valor passado, um numerador/deominador)
  */
 class Fracao
 {
@@ -13,44 +18,85 @@ class Fracao
 		int _numerador;
 		int _denominador;
 	public:
+		/**
+		 * @brief Construct a new Fracao object
+		 * 
+		 * @param numerador 
+		 * @param denominador 
+		 */
 		Fracao(int numerador, int denominador)
 		{
 			_numerador = numerador;
 			_denominador = denominador;
 		};
 
+		/**
+		 * @brief Construct a new Fracao object
+		 * 
+		 * @param numerador 
+		 */
 		Fracao(int numerador)
 		{
 			_numerador = numerador;
 			_denominador = 1;
 		};
 
+		/**
+		 * @brief Construct a new Fracao object
+		 * 
+		 */
 		Fracao()
 		{
 			_numerador = 0;
 			_denominador = 1;
 		};
 
+		/**
+		 * @brief Get the Numerador object
+		 * 
+		 * @return int 
+		 */
 		int getNumerador()
 		{
 			return _numerador;
 		};
 
+		/**
+		 * @brief Get the Denominador object
+		 * 
+		 * @return int 
+		 */
 		int getDenominador()
 		{
 			return _denominador;
 		};
 
+		/**
+		 * @brief Set the Numerador object
+		 * 
+		 * @param numerador 
+		 */
 		void setNumerador(int numerador)
 		{
 			_numerador = numerador;
 		};
 
+		/**
+		 * @brief Set the Denominador object
+		 * 
+		 * @param denominador 
+		 */
 		void setDenominador(int denominador)
 		{
 			_denominador = denominador;
 		};
 
+		/**
+		 * @brief Adiciona duas frações
+		 * 
+		 * @param Fracao fracao 
+		 * @return Fracao 
+		 */
 		Fracao soma(Fracao fracao)
 		{
 			Fracao resultado;
@@ -59,6 +105,12 @@ class Fracao
 			return resultado;
 		};
 
+		/**
+		 * @brief Subtrai duas frações
+		 * 
+		 * @param Fracao fracao 
+		 * @return Fracao 
+		 */
 		Fracao subtracao(Fracao fracao)
 		{
 			Fracao resultado;
@@ -67,6 +119,12 @@ class Fracao
 			return resultado;
 		};
 
+		/**
+		 * @brief Multiplica duas frações
+		 * 
+		 * @param Fracao fracao 
+		 * @return Fracao 
+		 */
 		Fracao multiplicacao(Fracao fracao)
 		{
 			Fracao resultado;
@@ -75,6 +133,12 @@ class Fracao
 			return resultado;
 		};
 
+		/**
+		 * @brief Divide duas frações
+		 * 
+		 * @param Fracao fracao 
+		 * @return Fracao 
+		 */
 		Fracao divisao(Fracao fracao)
 		{
 			Fracao resultado;
