55a2932f36e09ef354f0d7e8f21823f295bc0c07
wsl_srgoogle23 <leonardoliveira@ufmg.br>
2022-04-13 11:22:01 -0300

    feat: adicionado simulador de bst

---
+++ BST/bst.cpp
@@ -0,0 +1,104 @@
+/**
+ * @file bst.cpp
+ * @author Leonardo Oliveira (contato@leonardoliveira.com)
+ * @brief Implementação de arvore binária de busca (BST) com exemplicações de valores e testes.
+ * @version 0.1
+ * @date 2022-04-13
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+ 
+class BST {
+    int data;
+    BST *left, *right;
+ 
+public:
+    // Default constructor.
+    BST();
+ 
+    // Parameterized constructor.
+    BST(int);
+ 
+    // Insert function.
+    BST* Insert(BST*, int);
+ 
+    // Inorder traversal.
+    void Inorder(BST*);
+};
+ 
+// Default Constructor definition.
+BST ::BST()
+    : data(0)
+    , left(NULL)
+    , right(NULL)
+{
+}
+ 
+// Parameterized Constructor definition.
+BST ::BST(int value)
+{
+    data = value;
+    left = right = NULL;
+}
+ 
+// Insert function definition.
+BST* BST ::Insert(BST* root, int value)
+{
+    if (!root) {
+        // Insert the first node, if root is NULL.
+        return new BST(value);
+    }
+ 
+    // Insert data.
+    if (value > root->data) {
+        // Insert right node data, if the 'value'
+        // to be inserted is greater than 'root' node data.
+ 
+        // Process right nodes.
+        root->right = Insert(root->right, value);
+    }
+    else {
+        // Insert left node data, if the 'value'
+        // to be inserted is greater than 'root' node data.
+ 
+        // Process left nodes.
+        root->left = Insert(root->left, value);
+    }
+ 
+    // Return 'root' node, after insertion.
+    return root;
+}
+ 
+// Inorder traversal function.
+// This gives data in sorted order.
+void BST ::Inorder(BST* root)
+{
+    if (!root) {
+        return;
+    }
+    Inorder(root->left);
+    cout << root->data << endl;
+    Inorder(root->right);
+}
+ 
+
+int main()
+{
+    BST b, *root = NULL;
+    root = b.Insert(root, 9);
+    b.Insert(root, 7);
+    b.Insert(root, 0);
+    b.Insert(root, 5);
+    b.Insert(root, 1);
+    b.Insert(root, 3);
+    b.Insert(root, 8);
+    b.Insert(root, 6);
+    b.Insert(root, 4);
+    b.Insert(root, 2);
+    b.Inorder(root);
+    return 0;
+}
